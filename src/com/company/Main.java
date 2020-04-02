34b00852e32897a2f57d3c804b28a22a8a240482
Alexander Minchev <aleksander.minchev18@gmail.com>
2020-04-02 17:19:31 +0200

    Connecting to DB  + password setter query

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,26 @@
 package com.company;
 
+import java.sql.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.mainMenu();
+    public static void main(String[] args)  {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url="jdbc:mysql://localhost:3306/carrental?characterEncoding=latin1&useConfigs=maxPerformance\n";
+            String user="root";
+            String password="asdfzxcv123";
+            Connection myConn = DriverManager.getConnection(url, user, password);
+            Statement mySt= myConn.createStatement();
+            ResultSet rs= mySt.executeQuery("SELECT * FROM cartypes");
+            while (rs.next()) {
+                System.out.println(rs.getString("carType_id")+rs.getString("car_type"));
+            }
+        }
+        catch (SQLException | ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
