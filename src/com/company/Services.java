b9ff71e189be707890dd93c8127e130bcfaf7f9f
Alexander Minchev <aleksander.minchev18@gmail.com>
2020-04-04 13:00:46 +0200

    Create Contract and Customer methods

--- src/com/company/Services.java
+++ src/com/company/Services.java
@@ -11,6 +11,9 @@ public class Services {
     }
 
     public void createCar() {
+        /*Method for creating car elements then adding them to the system
+        */
+        //User input for creating a car element
         System.out.println("Enter brand ID:");
         int brand_id=console.nextInt();
         System.out.println("Enter Registration number: ");
@@ -21,6 +24,8 @@ public class Services {
         int mileage=console.nextInt();
         System.out.println("Enter car type: ");
         int car_type_id=console.nextInt();
+
+        //enters the user input into the db
         DBconnection.executeQuery("INSERT INTO cars(brandModel_id,reg_num,reg_date,mileage,carType_id)\n VALUES"+
                 "(" + brand_id + ",\"" + reg_num + "\",\"" + reg_date + "\", " + mileage + ", " + car_type_id + ");");
 
@@ -51,8 +56,33 @@ public class Services {
 
     }
     public void createCustomer() {
+        /*
+           Method for creating a customer elements in the DB then adding them to the system
+         */
+    System.out.println("Enter first name:");
+    String first_name=console.next();
+    System.out.println("Enter family name: ");
+    String last_name=console.next();
+    System.out.println("Enter zip code: ");
+    int zip=console.nextInt();
+    console.nextLine();
+    System.out.println("Enter address: ");
+    String address=console.nextLine();
+    System.out.println("Enter mobile number: ");
+    String number=console.next();
+    System.out.println("Enter alt. number: ");
+    String alt_number=console.next();
+    System.out.println("Enter email: ");
+    String email=console.next();
+    System.out.println("Enter driver licence number: ");
+    String driver_license=console.next();
+    System.out.println("Enter driver since date: ");
+    String driver_since=console.next();
 
-
+    //enters the user input into the DB
+        DBconnection.executeQuery("INSERT INTO customers(first_name,last_name,st_address,zip,mobile_phone,addit_phone,email,driver_license,driver_since)\n " +
+                "VALUES"+"(\""+first_name+"\",\""+last_name+"\",\""+address+"\","+zip+",\""+number+"\",\""+alt_number+"\",\""+
+                email+"\",\""+driver_license+"\",\""+driver_since+"\");");
     }
 
     public void displayCustomer() {
@@ -69,8 +99,21 @@ public class Services {
     }
 
     public void createRental() {
-
-
+        System.out.println("Enter renter id: ");
+        int renter_id=console.nextInt();
+        System.out.println("Enter car id: ");
+        int customer_id=console.nextInt();
+        console.nextLine();
+        System.out.println("Enter start date with hour(YYYY-MM-DD HH-MM-SS): ");
+        String start_date=console.nextLine();
+        System.out.println("Enter end date with hour(YYYY-MM-DD HH-MM-SS): ");
+        String end_date=console.nextLine();
+        System.out.println("Enter max km: ");
+        int max_km=console.nextInt();
+        System.out.println("Enter start_km: ");
+        int start_km=console.nextInt();
+        DBconnection.executeQuery("INSERT INTO contracts(renter_id,car_id,start_date,end_date,max_km,start_km) \n VALUES "+
+                "("+renter_id+","+customer_id+",\""+start_date+"\",\""+end_date+"\","+max_km+","+start_km+");");
     }
 
     public void displayRental() {
